f504d90882c3b1e0a08527c3f73f620d54ca7bea
memeliciouss <rakshit3raj@gmail.com>
2024-12-26 18:06:06 +0530

    views & templates

--- LocalHive/urls.py
+++ LocalHive/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+#to change admin text
+admin.site.site_header = "LocalHive Admin"
+admin.site.site_title = "LocalHive Admin Portal"
+admin.site.index_title = "Welcome to LocalHive Portal"
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls'))
 ]

00f9a5ae008c93a509c4d939188434a17d6a5253
Rakshit Raj <154298128+memeliciouss@users.noreply.github.com>
2024-12-26 17:36:26 +0000

    added create and view events on home page functionality

--- events/urls.py
+++ events/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from events import views
 
 urlpatterns = [
+    path("",views.index, name="home"),
     path("create/",views.create, name="createEvent")
 ]
--- events/views.py
+++ events/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from events.models import Events
 
 # Create your views here.
+def index(request):
+    events=Events.objects.all()
+
+    return render(request, 'index.html', {'events':events})
+
+
 def create(request):
+    if request.method == 'POST':
+        eventName = request.POST.get('eventName')
+        sport = request.POST.get('sport')
+        desc = request.POST.get('desc')
+        date = request.POST.get('date')
+        location = request.POST.get('location')
+        event=Events(eventName=eventName, sport=sport, desc=desc, date=date, location=location)
+        event.save()
+        return redirect('/')
+
     return render(request, 'create.html')
